247658964d135d012da077fca319541f5b0f799f
Kevin Li <kevinli1258@gmail.com>
2023-11-10 20:26:26 -0800

    first commit

---
+++ src/main/java/com/bet/betwebservice/BetWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bet.betwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bet/betwebservice/BetWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bet.betwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
